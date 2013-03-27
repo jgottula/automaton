2c7cc3a680fdbbb14df6779600148885b546a933
Justin Gottula <justin@jgottula.com>
2013-03-27 00:31:53 -0700

    io: replace inline functions with preprocessor macros

--- src/io.h
+++ src/io.h
@@ -12,35 +12,10 @@
 #include "std.h"
 
 
-typedef volatile uint8_t *reg8_t;
-typedef volatile uint16_t *reg16_t;
-
-
-static inline uint8_t _io_read(reg8_t reg, uint8_t mask) {
-	return *reg & mask;
-}
-
-static inline uint16_t _io_read16(reg16_t reg, uint16_t mask) {
-	return *reg & mask;
-}
-
-static inline void _io_write(reg8_t reg, uint8_t mask, uint8_t val) {
-	*reg = (*reg & ~mask) | (val & mask);
-}
-
-static inline void _io_write16(reg16_t reg, uint16_t mask, uint16_t val) {
-	*reg = (*reg & ~mask) | (val & mask);
-}
-
-
 #define io_read(_reg, _mask) \
-	_io_read(&_reg, _mask)
-#define io_read16(_reg, _mask) \
-	_io_read16(&_reg, _mask)
+	(_reg & _mask)
 #define io_write(_reg, _mask, _val) \
-	_io_write(&_reg, _mask, _val)
-#define io_write16(_reg, _mask, _val) \
-	_io_write16(&_reg, _mask, _val)
+	(_reg) = (((_reg) & ~(_mask)) | ((_val) & (_mask)))
 
 
 #endif
