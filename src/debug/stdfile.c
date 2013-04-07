38c4f79e3068620fac636d99efdf4ddad8041aa8
Justin Gottula <justin@jgottula.com>
2013-04-06 20:07:31 -0700

    debug/stdfile: set stdin and stdout to use the debug uart

---
+++ src/debug/stdfile.c
@@ -0,0 +1,38 @@
+/* automaton
+ * (c) 2013 Justin Gottula
+ * The source code of this project is distributed under the terms of the
+ * simplified BSD license. See the LICENSE file for details.
+ */
+
+
+#include "debug/stdfile.h"
+#include "io/uart.h"
+
+
+static int stdin_get(FILE *f) {
+	char c;
+	if (uart_read_chr(UART_DEBUG, &c) != 0) {
+		return (int)c;
+	} else {
+		return _FDEV_ERR;
+	}
+}
+
+static int stdout_put(char c, FILE *f) {
+	if (uart_write_chr(UART_DEBUG, c) != 0) {
+		return 0;
+	} else {
+		return _FDEV_ERR;
+	}
+}
+
+
+void stdfile_open(void) {
+	(void)fdevopen(NULL, stdin_get);
+	(void)fdevopen(stdout_put, NULL);
+}
+
+void stdfile_close(void) {
+	fclose(stdout);
+	fclose(stdin);
+}
---
+++ src/debug/stdfile.h
@@ -0,0 +1,19 @@
+/* automaton
+ * (c) 2013 Justin Gottula
+ * The source code of this project is distributed under the terms of the
+ * simplified BSD license. See the LICENSE file for details.
+ */
+
+
+#ifndef AUTOMATON_DEBUG_STDFILE_H
+#define AUTOMATON_DEBUG_STDFILE_H
+
+
+#include "std.h"
+
+
+void stdfile_open(void);
+void stdfile_close(void);
+
+
+#endif
