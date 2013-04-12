2d60c18659edd343770ac72296725ac1d1934990
Justin Gottula <justin@jgottula.com>
2013-04-12 14:50:47 -0700

    rtc/ds1302: add stub

---
+++ src/rtc/ds1302.c
@@ -0,0 +1,13 @@
+/* automaton
+ * (c) 2013 Justin Gottula
+ * The source code of this project is distributed under the terms of the
+ * simplified BSD license. See the LICENSE file for details.
+ */
+
+
+#include "rtc/ds1302.h"
+
+
+void ds1302_(void) {
+	
+}
---
+++ src/rtc/ds1302.h
@@ -0,0 +1,18 @@
+/* automaton
+ * (c) 2013 Justin Gottula
+ * The source code of this project is distributed under the terms of the
+ * simplified BSD license. See the LICENSE file for details.
+ */
+
+
+#ifndef AUTOMATON_RTC_DS1302_H
+#define AUTOMATON_RTC_DS1302_H
+
+
+#include "std.h"
+
+
+
+
+
+#endif
