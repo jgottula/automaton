8946acbda6d2bb48c70b6b0ccaeecffc81aeed0e
Justin Gottula <justin@jgottula.com>
2013-04-19 17:07:56 -0700

    time/delay: add delay cycle macros for 10 MHz

--- src/time/delay.h
+++ src/time/delay.h
@@ -12,11 +12,15 @@
 #include "std.h"
 
 
-_Static_assert(F_CPU == 20000000UL, "delay values are wrong");
-
-
+#if F_CPU == 20000000UL
 #define delay_150ns()  __builtin_avr_delay_cycles(2)
 #define delay_1200ns() __builtin_avr_delay_cycles(23)
+#elif F_CPU == 10000000UL
+#define delay_150ns()  __builtin_avr_delay_cycles(1)
+#define delay_1200ns() __builtin_avr_delay_cycles(11)
+#else
+#error delay cycle counts are wrong
+#endif
 
 
 #endif
