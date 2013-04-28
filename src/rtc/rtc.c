b01e54efe883f97bf7529ba43bc21aab4c1dbd1b
Justin Gottula <justin@jgottula.com>
2013-04-27 17:19:01 -0700

    rtc/rtc: implement rtc_set_tm

--- src/rtc/rtc.c
+++ src/rtc/rtc.c
@@ -23,7 +23,6 @@ void rtc_get_tm(struct tm *tm) {
 	tm->tm_sec  = bcd_decode_packed(ck[DS1302_CK_SEC] & 0x7f);
 	tm->tm_min  = bcd_decode_packed(ck[DS1302_CK_MIN]);
 	tm->tm_hour = bcd_decode_packed(ck[DS1302_CK_HOUR]);
-	
 	tm->tm_mday = bcd_decode_packed(ck[DS1302_CK_DATE]);
 	tm->tm_wday = bcd_decode_packed(ck[DS1302_CK_DAY]) - 1;
 	tm->tm_mon  = bcd_decode_packed(ck[DS1302_CK_MONTH]) - 1;
@@ -35,5 +34,17 @@ void rtc_get_tm(struct tm *tm) {
 }
 
 void rtc_set_tm(const struct tm *tm) {
-#warning TODO
+	uint8_t ck[8];
+	
+	ck[DS1302_CK_SEC]   = bcd_encode_packed(tm->tm_sec);
+	ck[DS1302_CK_MIN]   = bcd_encode_packed(tm->tm_min);
+	ck[DS1302_CK_HOUR]  = bcd_encode_packed(tm->tm_hour);
+	ck[DS1302_CK_DATE]  = bcd_encode_packed(tm->tm_mday);
+	ck[DS1302_CK_MONTH] = bcd_encode_packed(tm->tm_mon + 1);
+	ck[DS1302_CK_DAY]   = bcd_encode_packed(tm->tm_wday + 1);
+	ck[DS1302_CK_YEAR]  = bcd_encode_packed(tm->tm_year - 100);
+	
+	ck[DS1302_CK_WR_PROT] = 0x00;
+	
+	ds1302_ck_write_all(ck);
 }
