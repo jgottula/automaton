c6d733edfc614c1417a3a5560d097d237bd6c99b
Justin Gottula <justin@jgottula.com>
2013-04-29 00:05:42 -0700

    rtc/rtc: filter incoming data bits

--- src/rtc/rtc.c
+++ src/rtc/rtc.c
@@ -24,13 +24,13 @@ void rtc_get_tm(struct tm *tm) {
 	ds1302_ck_read_all(ck);
 	
 	/* no handling for 12-hour time; you get what you deserve if you use that */
-	tm->tm_sec  = bcd_decode_packed(ck[DS1302_CK_SEC] & 0x7f);
-	tm->tm_min  = bcd_decode_packed(ck[DS1302_CK_MIN]);
-	tm->tm_hour = bcd_decode_packed(ck[DS1302_CK_HOUR]);
-	tm->tm_mday = bcd_decode_packed(ck[DS1302_CK_DATE]);
-	tm->tm_wday = bcd_decode_packed(ck[DS1302_CK_DAY]) - 1;
-	tm->tm_mon  = bcd_decode_packed(ck[DS1302_CK_MONTH]) - 1;
-	tm->tm_year = bcd_decode_packed(ck[DS1302_CK_YEAR]) + 100;
+	tm->tm_sec  = bcd_decode_packed(0x7f & ck[DS1302_CK_SEC]);
+	tm->tm_min  = bcd_decode_packed(0x7f & ck[DS1302_CK_MIN]);
+	tm->tm_hour = bcd_decode_packed(0x3f & ck[DS1302_CK_HOUR]);
+	tm->tm_mday = bcd_decode_packed(0x3f & ck[DS1302_CK_DATE]);
+	tm->tm_mon  = bcd_decode_packed(0x1f & ck[DS1302_CK_MONTH]) - 1;
+	tm->tm_wday = bcd_decode_packed(0x07 & ck[DS1302_CK_DAY]) -  1;
+	tm->tm_year = bcd_decode_packed(0xff & ck[DS1302_CK_YEAR]) + 100;
 	
 	/* we don't set tm->tm_yday */
 	
@@ -63,13 +63,13 @@ void rtc_get_tm(struct tm *tm) {
 void rtc_set_tm(const struct tm *tm) {
 	uint8_t ck[8];
 	
-	ck[DS1302_CK_SEC]   = bcd_encode_packed(tm->tm_sec);
-	ck[DS1302_CK_MIN]   = bcd_encode_packed(tm->tm_min);
-	ck[DS1302_CK_HOUR]  = bcd_encode_packed(tm->tm_hour);
-	ck[DS1302_CK_DATE]  = bcd_encode_packed(tm->tm_mday);
-	ck[DS1302_CK_MONTH] = bcd_encode_packed(tm->tm_mon + 1);
-	ck[DS1302_CK_DAY]   = bcd_encode_packed(tm->tm_wday + 1);
-	ck[DS1302_CK_YEAR]  = bcd_encode_packed(tm->tm_year - 100);
+	ck[DS1302_CK_SEC]   = 0x7f & bcd_encode_packed(tm->tm_sec);
+	ck[DS1302_CK_MIN]   = 0x7f & bcd_encode_packed(tm->tm_min);
+	ck[DS1302_CK_HOUR]  = 0x3f & bcd_encode_packed(tm->tm_hour);
+	ck[DS1302_CK_DATE]  = 0x3f & bcd_encode_packed(tm->tm_mday);
+	ck[DS1302_CK_MONTH] = 0x1f & bcd_encode_packed(tm->tm_mon + 1);
+	ck[DS1302_CK_DAY]   = 0x07 & bcd_encode_packed(tm->tm_wday + 1);
+	ck[DS1302_CK_YEAR]  = 0xff & bcd_encode_packed(tm->tm_year - 100);
 	
 	ck[DS1302_CK_WR_PROT] = 0x00;
 	
