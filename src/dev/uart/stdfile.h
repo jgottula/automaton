c53e72f629466639a013d9757df1e7e7c2323113
Justin Gottula <justin@jgottula.com>
2014-02-27 13:04:57 -0800

    dev/uart/stdfile: port over and remove non-FTDI parts

--- src/dev/uart/stdfile.h
+++ src/dev/uart/stdfile.h
@@ -1,19 +1,15 @@
 /* automaton
- * (c) 2013 Justin Gottula
+ * (c) 2014 Justin Gottula
  * The source code of this project is distributed under the terms of the
  * simplified BSD license. See the LICENSE file for details.
  */
 
 
-#ifndef AUTOMATON_DEBUG_STDFILE_H
-#define AUTOMATON_DEBUG_STDFILE_H
+#ifndef AUTOMATON_DEV_UART_STDFILE_H
+#define AUTOMATON_DEV_UART_STDFILE_H
 
 
 #include "std.h"
-
-
-extern FILE *lcd;
-extern FILE *stn1110;
 
 
 void stdfile_open(void);
