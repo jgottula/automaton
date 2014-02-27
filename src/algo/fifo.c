fa5fd372ce67c443fc5cdd301977a12dccf82c9c
Justin Gottula <justin@jgottula.com>
2014-02-27 12:55:40 -0800

    algo/fifo: put atomic responsibility onto fifo_ function callers

--- src/algo/fifo.c
+++ src/algo/fifo.c
@@ -8,7 +8,7 @@
 #include "algo/fifo.h"
 
 
-/* nonatomic: push without any checks whatsoever */
+/* push without any checks whatsoever */
 static void _fifo_push(struct fifo *fifo, uint8_t val) {
 	fifo->data[fifo->i_push] = val;
 	
@@ -20,7 +20,7 @@ static void _fifo_push(struct fifo *fifo, uint8_t val) {
 	++fifo->count;
 }
 
-/* nonatomic: pop without any checks whatsoever */
+/* pop without any checks whatsoever */
 static uint8_t _fifo_pop(struct fifo *fifo) {
 	uint8_t val = fifo->data[fifo->i_pop];
 	
@@ -35,7 +35,7 @@ static uint8_t _fifo_pop(struct fifo *fifo) {
 }
 
 
-/* nonatomic: initialize fifo */
+/* initialize fifo */
 void fifo_init(struct fifo *fifo, uint8_t size) {
 	fifo->i_push = 0;
 	fifo->i_pop  = 0;
@@ -46,89 +46,87 @@ void fifo_init(struct fifo *fifo, uint8_t size) {
 	fifo->data = malloc(size);
 }
 
-/* nonatomic: deinitialize fifo */
+/* deinitialize fifo */
 void fifo_deinit(struct fifo *fifo) {
 	free(fifo->data);
 }
 
 
-/* atomic: try to push a value onto a fifo; returns false if full */
+/* NONATOMIC: try to push a value onto a fifo; returns false if full */
 bool fifo_push(struct fifo *fifo, uint8_t val) {
-	bool result = false;
-	
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		if (fifo->count < fifo->size) {
-			_fifo_push(fifo, val);
-			result = true;
-		}
+	if (fifo->count < fifo->size) {
+		_fifo_push(fifo, val);
+		return true;
+	} else {
+		return false;
 	}
-	
-	return result;
 }
 
-/* atomic: try to pop a value off of a fifo; returns false if empty */
+/* NONATOMIC: try to pop a value off of a fifo; returns false if empty */
 bool fifo_pop(struct fifo *fifo, uint8_t *out) {
-	bool result = false;
-	
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		if (fifo->count > 0) {
-			*out = _fifo_pop(fifo);
-			result = true;
-		}
+	if (fifo->count > 0) {
+		*out = _fifo_pop(fifo);
+		return true;
+	} else {
+		return false;
 	}
-	
-	return result;
 }
 
 
-/* atomic: try to push two values onto a fifo; returns false if full */
+/* NONATOMIC: try to push two values onto a fifo; returns false if full */
 bool fifo_push16(struct fifo *fifo, uint16_t val) {
-	bool result = false;
-	
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		if (fifo->count + 1 < fifo->size) {
-			_fifo_push(fifo, val);
-			_fifo_push(fifo, val >> 8);
-			result = true;
-		}
+	if (fifo->count + 1 < fifo->size) {
+		_fifo_push(fifo, val);
+		_fifo_push(fifo, val >> 8);
+		return true;
+	} else {
+		return false;
 	}
-	
-	return result;
 }
 
-/* atomic: try to pop two values off of a fifo; returns false if empty */
+/* NONATOMIC: try to pop two values off of a fifo; returns false if empty */
 bool fifo_pop16(struct fifo *fifo, uint16_t *out) {
-	bool result = false;
-	
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		if (fifo->count > 1) {
-			*out = (_fifo_pop(fifo) | ((uint16_t)_fifo_pop(fifo) << 8));
-			result = true;
-		}
+	if (fifo->count > 1) {
+		*out = (_fifo_pop(fifo) | ((uint16_t)_fifo_pop(fifo) << 8));
+		return true;
+	} else {
+		return false;
 	}
-	
-	return result;
 }
 
 
-/* atomic: push a value onto a fifo; removes the oldest value if full */
-void fifo_push_force(struct fifo *fifo, uint8_t val) {
+/* NONATOMIC: push a value onto a fifo; removes the oldest value if full;
+ * returns false if forceful action was necessary */
+bool fifo_push_force(struct fifo *fifo, uint8_t val) {
+	bool result = true;
+	
 	if (fifo->count >= fifo->size) {
 		/* pop into bit bucket */
 		(void)_fifo_pop(fifo);
+		
+		result = false;
 	}
 	
 	_fifo_push(fifo, val);
+	
+	return result;
 }
 
-/* atomic: push two values onto a fifo; removes the oldest value pair if full */
-void fifo_push16_force(struct fifo *fifo, uint16_t val) {
+/* NONATOMIC: push two values onto a fifo; removes the oldest pair if full;
+ * returns false if forceful action was necessary */
+bool fifo_push16_force(struct fifo *fifo, uint16_t val) {
+	bool result = true;
+	
 	if (fifo->count + 1 >= fifo->size) {
 		/* pop into bit bucket */
 		(void)_fifo_pop(fifo);
 		(void)_fifo_pop(fifo);
+		
+		result = false;
 	}
 	
 	_fifo_push(fifo, val);
 	_fifo_push(fifo, val >> 8);
+	
+	return result;
 }
