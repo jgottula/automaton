a21def389d87520b9351a458bbfc462e35a0261e
Justin Gottula <justin@jgottula.com>
2014-03-26 17:05:04 -0700

    obd/obd: use new can_msg struct with can_tx

--- src/obd/obd.c
+++ src/obd/obd.c
@@ -9,16 +9,33 @@
 #include "dev/can/can.h"
 
 
+static uint8_t pid_bitmask[32];
+
+
 void obd_test(void) {
 	printf_P(PSTR("obd_test()\n"));
 	uart_flush();
 	
 	can_init(500);
 	
-	uint8_t data[8] = {
-		2, 1, 0,
+	struct can_msg msg = {
+		.id = 0x7df,
+		
+		.dlc = 8,
+		.data = {
+			2, // len
+			1, // mode
+			0, // pid
+		},
 	};
-	can_tx(0x7df, 8, data);
+	
+	can_tx(&msg);
+	
+	/*_delay_ms(1);
+	data[2] = 4;
+	can_tx(0x7df, 8, data);*/
+	
+	// TODO: pid scan: chain 00, 20, 40... (until they end) to assemble bitmask
 	
 	for ( ;; );
 }
